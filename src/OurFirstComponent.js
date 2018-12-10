7491028f6e052e1264b780f155f8a4833294d6aa
Nelson Gutierrez <nelson.gutierrez@walmart.com>
2018-12-10 11:40:08 -0300

    create a new components

---
+++ src/OurFirstComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class OurFirstComponent extends Component {
+  render() {
+    return (
+        <h1>I am the child!</h1>
+    );
+  }
+}
+
+export default OurFirstComponent;
