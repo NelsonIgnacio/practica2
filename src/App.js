20423ff24f42150e760e056b3788ee0d7c0f8310
Nelson Gutierrez <nelson.gutierrez@walmart.com>
2018-12-10 12:13:07 -0300

    create a new components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import OurFirstComponent from './OurFirstComponent'
 import './App.css';
 
-class App extends Component {
+class Container extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isMusicPlaying: false }
+  }
+
+  PlayButton() {
+    return <a href="/" title="Play video" className="play"/>
+  };
   render() {
+    const status = this.state.isMusicPlaying ? 'Playing :)' : 'Not playing :(';
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <h1>{status}</h1>
+        <OurFirstComponent/>
       </div>
     );
   }
 }
 
-export default App;
+export default Container;
