4b50dedbf060ccf7046cdfed9c0aa29392b059f1
nacho <nelson.gutierrez@walmart.com>
2018-12-10 12:37:05 -0300

    fix code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import OurFirstComponent from './OurFirstComponent'
 import './App.css';
 
-class Container extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = { isMusicPlaying: false }
   }
 
   PlayButton() {
-    return <a href="/" title="Play video" className="play"/>
+    return (<a href="/" title="Play video" className="play"> </a>)
   };
   render() {
     const status = this.state.isMusicPlaying ? 'Playing :)' : 'Not playing :(';
@@ -18,9 +18,10 @@ class Container extends Component {
       <div className='App'>
         <h1>{status}</h1>
         <OurFirstComponent/>
+     {this.PlayButton()}   
       </div>
     );
   }
 }
 
-export default Container;
+export default App;
